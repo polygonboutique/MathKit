b1d5b8795bd68875bcd6ec310f38a8615e071f0b
Polygon Boutique <info@deadkitteninaframe.com>
2014-02-23 21:30:14 +0100

    multiplications

--- src/mathkit/float4x4.java
+++ src/mathkit/float4x4.java
@@ -442,12 +442,8 @@ public class float4x4 {
      * @return (mat * vector)
      */
     public float3 multiply(float3 v){
-        float homogenousDivideFactor = 1.0f / (m[3][0] * v.x + m[3][1] * v.y + m[3][2] * v.z + m[3][3]);
-
-        // now we multiply and divide by the homogenous coordinate (1)
-        return new float3(  (m[0][0] * v.x + m[0][1] * v.y + m[0][2] * v.z + m[0][3]) * homogenousDivideFactor,
-                            (m[1][0] * v.x + m[1][1] * v.y + m[1][2] * v.z + m[1][3]) * homogenousDivideFactor,
-                            (m[2][0] * v.x + m[2][1] * v.y + m[2][2] * v.z + m[2][3]) * homogenousDivideFactor);
+        float4 u = multiply(new float4(v, 1.0f));
+        return u.xyz().divide(u.w);
     }
 
     /**
--- src/tests/Main.java
+++ src/tests/Main.java
@@ -1,8 +1,6 @@
 package tests;
 
 import mathkit.float3;
-import mathkit.float3x3;
-import mathkit.float4;
 import mathkit.float4x4;
 
 public class Main {
@@ -10,9 +8,13 @@ public class Main {
     public static void main(String[] args) {
         float4x4 M = new float4x4();
 
-        M = M.initTranslationColumn(1,1,1);
+        M = M.initTranslationColumn(8,4,23);
+
+        float4x4 S = new float4x4().initScale(2,3,4);
+        M = M.multiply(S);
 
         float3 v = new float3(1, 2, 4);
+        System.out.println(M);
 
         System.out.println(M.multiply(v));
     }
